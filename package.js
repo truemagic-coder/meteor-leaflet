c1edc38a716eb763df474df62b5b9cbf35caba94
Bevan Hunt <me@bevanhunt.com>
2015-03-22 12:55:00 -0700

    covert package.js to 2 spaces

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
-    name: "bevanhunt:leaflet",
-    summary: "Leaflet.js, mobile-friendly interactive maps.",
-    git: "https://github.com/bevanhunt/meteor-leaflet.git",
-    author: "Bevan Hunt <bevan@bevanhunt.com> (http://www.bevanhunt.com)",
-    version: "0.3.12",
-    license: "MIT/X11"
+  name: "bevanhunt:leaflet",
+  summary: "Leaflet.js, mobile-friendly interactive maps.",
+  git: "https://github.com/bevanhunt/meteor-leaflet.git",
+  author: "Bevan Hunt <bevan@bevanhunt.com> (http://www.bevanhunt.com)",
+  version: "0.3.12",
+  license: "MIT/X11"
 });
 
 Package.onUse(function (api) {
+  api.versionsFrom('1.0');
 
-    api.versionsFrom('1.0');
-
-    api.addFiles([
-        'lib/leaflet.js',
-        'lib/leaflet_providers.js',
-        'styles/leaflet.css',
-        'images/layers-2x.png',
-        'images/layers.png',
-        'images/marker-icon-2x.png',
-        'images/marker-icon.png',
-        'images/marker-shadow.png'
-    ],'client');
+  api.addFiles([
+    'lib/leaflet.js',
+    'lib/leaflet_providers.js',
+    'styles/leaflet.css',
+    'images/layers-2x.png',
+    'images/layers.png',
+    'images/marker-icon-2x.png',
+    'images/marker-icon.png',
+    'images/marker-shadow.png'
+  ],'client');
 });
