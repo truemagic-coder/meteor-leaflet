e3e6d593dbc3ef7f1e0d260c9fcc706282c930e5
Bevan Hunt <bevan@bevanhunt.com>
2016-09-29 20:18:13 -0700

    bump version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: "leaflet - mobile-friendly maps.",
   git: "https://github.com/bevanhunt/meteor-leaflet.git",
   author: "Bevan Hunt <bevan@bevanhunt.com> (http://bevanhunt.com)",
-  version: "3.0.0",
+  version: "3.0.1",
   license: "MIT"
 });
 
